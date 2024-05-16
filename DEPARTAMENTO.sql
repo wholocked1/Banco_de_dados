9fd542b94ca3730452d6747e6ee3bdda2e3a8a72
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2024-05-15 21:38:04 -0300

    Update DEPARTAMENTO.sql

--- DEPARTAMENTO.sql
+++ DEPARTAMENTO.sql
@@ -1,6 +1,7 @@
-insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Línguas', 01, null);
-insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Gestão', 02, null);
-insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Marketing', 03, null);
-insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Ciências Sociais e Jurídico', 04, null);
-insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Arquitetura', 05, null);
-insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Matemática', 06, null);
+insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Línguas', 01, 7358);
+insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Gestão', 02, 3023);
+insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Marketing', 03, 9852);
+insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Ciências Sociais e Jurídico', 04, 3677);
+insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Arquitetura', 05, 9711);
+insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Matemática', 06, 3477);
+insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Artes', 07, 5275);
