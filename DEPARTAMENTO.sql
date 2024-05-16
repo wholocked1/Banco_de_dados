b3c58ad2e700b74b4bbecccc8c472940c805da1f
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2024-05-15 21:06:02 -0300

    Create DEPARTAMENTO.sql

---
+++ DEPARTAMENTO.sql
@@ -0,0 +1,6 @@
+insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Línguas', 01, null);
+insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Gestão', 02, null);
+insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Marketing', 03, null);
+insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Ciências Sociais e Jurídico', 04, null);
+insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Arquitetura', 05, null);
+insert into DEPARTAMENTO(nome, id_departamento, id_prof_chefe) values ('Matemática', 06, null);
