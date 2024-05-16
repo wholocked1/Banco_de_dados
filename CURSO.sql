c12c15f72d4cdbd5fa4f590e4c8a9479ba635b19
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2024-05-15 21:25:28 -0300

    Create CURSO.sql

---
+++ CURSO.sql
@@ -0,0 +1,6 @@
+insert into CURSO(nome, duração, id_departamento) values ('Artes', 1, 07);
+insert into CURSO(nome, duração, id_departamento) values ('Marketing', 3, 03);
+insert into CURSO(nome, duração, id_departamento) values ('Gestão', 2, 02);
+insert into CURSO(nome, duração, id_departamento) values ('Arquitetura', 4, 05);
+
+
