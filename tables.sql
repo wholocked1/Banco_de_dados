8bf829edf5ff44cac085ae420460b4bbddea2fc6
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2024-05-16 22:00:19 -0300

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,4 +1,4 @@
-CREATE SCHEMA faculdade --criação do schema
+CREATE SCHEMA faculdade; --criação do schema
 CREATE TABLE faculdade.aluno ( --criação da tabela de aluno
   nome TEXT,
   id_aluno INT PRIMARY KEY,
@@ -6,46 +6,46 @@ CREATE TABLE faculdade.aluno ( --criação da tabela de aluno
   formado BOOLEAN,
   id_tcc INT,
   id_curso INT
-)
+);
 CREATE TABLE faculdade.curso ( --criação da tabela de curso
   duracao INT,
   id_depart INT,
   id_curso INT PRIMARY KEY,
   nome TEXT
-)
+);
 CREATE TABLE faculdade.departamento ( --criação da tabela de departamento
   id_depart INT PRIMARY KEY,
   id_prof_chefe INT,
   nome TEXT
-)
+);
 CREATE TABLE faculdade.tcc ( --criação da tabela de tcc
   id_tcc INT PRIMARY KEY,
   id_prof INT,
   titulo TEXT
-)
+);
 CREATE TABLE faculdade.professor ( --criação da tabela de professor
   nome TEXT,
   id_depart INT,
   id_prof INT PRIMARY KEY
-)
+);
 CREATE TABLE faculdade.hist_prof ( --criação da tabela de histórico do professor
   id_prof INT,
   id_disc INT,
   semestre INT,
   ano INT
-)
+);
 CREATE TABLE faculdade.hist_aluno ( --criação da tabela de histório de aluno
   id_aluno INT,
   id_disc INT,
   semestre INT,
   ano INT,
   nota DECIMAL(4,2)
-)
+);
 CREATE TABLE faculdade.matriz ( --criação da tabela de matriz
   id_disc INT,
   id_curso INT,
   semestre INT
-)
+);
 CREATE TABLE faculdade.disciplina ( --criação da tabela de disciplina
   nome TEXT,
   codigo TEXT PRIMARY KEY,
