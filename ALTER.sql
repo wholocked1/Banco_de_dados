20eba86595e1cb3dd11f63b214770ac6d0cc4941
Enzo-Cassin <148575140+Enzo-Cassin@users.noreply.github.com>
2024-05-16 22:07:42 -0300

    Update ALTER.sql

--- ALTER.sql
+++ ALTER.sql
@@ -1,6 +1,8 @@
 ALTER TABLE faculdade.aluno
   ADD CONSTRAINT id_tcc FOREIGN KEY (id_tcc)
       REFERENCES faculdade.tcc
+;
+ALTER TABLE faculdade.aluno
   ADD CONSTRAINT id_curso FOREIGN KEY (id_curso)
       REFERENCES faculdade.curso
 ;
@@ -23,18 +25,24 @@ ALTER TABLE faculdade.professor
 ALTER TABLE faculdade.hist_prof
   ADD CONSTRAINT id_prof FOREIGN KEY (id_prof)
     REFERENCES faculdade.professor
+;
+ALTER TABLE faculdade.hist_prof
   ADD CONSTRAINT id_disc FOREIGN KEY (id_disc)
     REFERENCES faculdade.disciplina
 ;
 ALTER TABLE faculdade.hist_aluno
   ADD CONSTRAINT id_aluno FOREIGN KEY (id_aluno)
     REFERENCES faculdade.aluno
+;
+ALTER TABLE faculdade.hist_aluno
   ADD CONSTRAINT id_disc FOREIGN KEY (id_disc)
     REFERENCES faculdade.disciplina
 ;
 ALTER TABLE faculdade.matriz
   ADD CONSTRAINT id_disc FOREIGN KEY (id_disc)
     REFERENCES faculdade.disciplina
+;
+ALTER TABLE faculdade.matriz
   ADD CONSTRAINT id_curso FOREIGN KEY (id_curso)
       REFERENCES faculdade.curso
 ;
