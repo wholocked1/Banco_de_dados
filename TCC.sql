5aab613e685ae09471cb15e0f2be3a5183be1edc
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2024-05-16 21:03:35 -0300

    Update TCC.sql

--- TCC.sql
+++ TCC.sql
@@ -9,3 +9,12 @@ insert into TCC (id_tcc, id_prof, titulo) values (1107, 4481, 'sed ante');
 insert into TCC (id_tcc, id_prof, titulo) values (4251, 4481, 'justo eu massa donec');
 insert into TCC (id_tcc, id_prof, titulo) values (1544, 5275, 'rhoncus dui vel');
 
+insert into TCC (id_tcc, id_prof, titulo) values ('7977', '4481', 'vivamus vestibulum sagittis sapien');
+insert into TCC (id_tcc, id_prof, titulo) values ('2766', '3477', 'cubilia curae');
+insert into TCC (id_tcc, id_prof, titulo) values ('2996', '9852', 'diam cras pellentesque volutpat');
+insert into TCC (id_tcc, id_prof, titulo) values ('4085', '3477', 'pede posuere');
+insert into TCC (id_tcc, id_prof, titulo) values ('7193', '0673', 'dapibus nulla suscipit ligula');
+insert into TCC (id_tcc, id_prof, titulo) values ('0846', '9852', 'in imperdiet et');
+insert into TCC (id_tcc, id_prof, titulo) values ('4594', '0673', 'augue vestibulum ante');
+insert into TCC (id_tcc, id_prof, titulo) values ('4639', '7358', 'ultricies eu');
+
