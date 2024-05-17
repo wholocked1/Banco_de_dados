86f642ca6c0a13153f7618004682a28b03d5db1d
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2024-05-16 21:01:00 -0300

    Create TCC.sql

---
+++ TCC.sql
@@ -0,0 +1,11 @@
+insert into TCC (id_tcc, id_prof, titulo) values (3094, 7358, 'ipsum primis');
+insert into TCC (id_tcc, id_prof, titulo) values (5380, 3677, 'eget eros elementum');
+insert into TCC (id_tcc, id_prof, titulo) values (5451, 5275, 'tellus nisi eu');
+insert into TCC (id_tcc, id_prof, titulo) values (9171, 7358, 'habitasse platea dictumst');
+insert into TCC (id_tcc, id_prof, titulo) values (6334, 3677, 'ante nulla justo aliquam');
+insert into TCC (id_tcc, id_prof, titulo) values (2767, 3677, 'ac nibh fusce lacus');
+insert into TCC (id_tcc, id_prof, titulo) values (7372, 7358, 'ac diam');
+insert into TCC (id_tcc, id_prof, titulo) values (1107, 4481, 'sed ante');
+insert into TCC (id_tcc, id_prof, titulo) values (4251, 4481, 'justo eu massa donec');
+insert into TCC (id_tcc, id_prof, titulo) values (1544, 5275, 'rhoncus dui vel');
+
