c763d555653415300e7be4fc0293589eb9db101f
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2024-05-22 21:07:57 -0300

    Create Query_1.sql

---
+++ Query_1.sql
@@ -0,0 +1,4 @@
+-- histórico escolar de qualquer aluno, retornando o código e nome da disciplina, semestre e ano que a disciplina foi cursada e nota final
+
+SELECT * FROM faculdade.hist_aluno
+WHERE id_aluno = 1530;
